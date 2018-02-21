5f1508fc4cee13985611c79db0db12b74628f958
DK\obr <obr@sitecore.net>
2018-02-21 13:41:46 +0200

    Init

---
+++ src/Sitecore.Support.93391/Resources/Media/MediaRequest.cs
@@ -0,0 +1,32 @@
+﻿namespace Sitecore.Support.Resources.Media
+{
+    using Sitecore.Reflection;
+    using Sitecore.Resources.Media;
+    using Sitecore.Web;
+    using System;
+    using System.Web;
+
+    public class MediaRequest : Sitecore.Resources.Media.MediaRequest
+    {
+        public override Sitecore.Resources.Media.MediaRequest Clone()
+        {
+            Sitecore.Support.Resources.Media.MediaRequest url = new Sitecore.Support.Resources.Media.MediaRequest();
+            ReflectionUtil.SetField(url, "innerRequest", ReflectionUtil.GetField(this, "innerRequest"));
+            ReflectionUtil.SetField(url, "mediaUri", ReflectionUtil.GetField(this, "mediaUri"));
+            ReflectionUtil.SetField(url, "options", ReflectionUtil.GetField(this, "options"));
+            ReflectionUtil.SetField(url, "mediaQueryString", ReflectionUtil.GetField(this, "mediaQueryString"));
+            return url;
+        }
+
+        protected override string GetMediaPath()
+        {
+            string rawUrl = Context.RawUrl;
+            if (WebUtil.Is404Request(rawUrl))
+            {
+                rawUrl = WebUtil.GetRequestUri404(rawUrl);
+            }
+            string localPath = WebUtil.GetLocalPath(rawUrl);
+            return this.GetMediaPath(localPath);
+        }
+    }
+}
